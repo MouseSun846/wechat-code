4f5fd1e311b6187daea0b5294dd9321678944054
3026098675@qq.com <3026098675@qq.com>
2026-01-17 14:24:11 +0800

    refactor(RedisConfig): 优化Redis序列化配置代码结构
    将Jackson2JsonRedisSerializer的初始化与ObjectMapper配置合并，简化代码并提高可读性

--- src/main/java/com/wechat/passcode/config/RedisConfig.java
+++ src/main/java/com/wechat/passcode/config/RedisConfig.java
@@ -35,14 +35,13 @@ public class RedisConfig {
         template.setConnectionFactory(connectionFactory);
 
         // JSON序列化配置
-        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = 
-            new Jackson2JsonRedisSerializer<>(Object.class);
-        
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, 
             ObjectMapper.DefaultTyping.NON_FINAL);
-        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = 
+            new Jackson2JsonRedisSerializer<>(objectMapper, Object.class);
 
         // String序列化配置
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
